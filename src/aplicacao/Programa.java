d96a92aa08a7535806ecba27ec1cc295935155fb
tlandim-land <tlandim@gmail.com>
2021-09-24 21:05:03 -0300

    criacao inicial tabuleiro

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.println("BEM VINDO AO JOGO... QUE VENÇA O MELHOR!");
+		
+		Posicao pos = new Posicao(3, 4);
+		
+		System.out.println(pos);
+	}
+
+}
